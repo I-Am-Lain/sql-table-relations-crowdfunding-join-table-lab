57b4956d357897b9dcadb15487c8ef7e3382f160
I-Am-Lain <vetter.william@gmail.com>
2020-09-23 15:39:54 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,44 @@
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("Fhqwgads", "HSR", 10000.0, "9/23/2020", "9/24/2020"),
+("Abba", "Art", 50, "1/1/1", "2/2/2"),
+("Beesely", "Band Music", 420.42, "2/22/2040", "3/22/2040"),
+("Catrina", "Cantrips", 69.69, "3/17/148", "1/1/2012"),
+("Dandy", "Dandelions", 48930832, "8/9/10", "9/10/11"),
+(null, null, null, null, null),
+(null, null, null, null, null),
+(null, null, null, null, null),
+(null, null, null, null, null),
+(null, null, null, null, null);
+
+INSERT INTO users(name, age) VALUES
+("Bob", 19),
+("Cindy", 65),
+(null, null),
+(null, null),
+(null, null),
+(null, null),
+(null, null),
+(null, null),
+(null, null),
+(null, null),
+(null, null),
+(null, null),
+(null, null),
+(null, null),
+(null, null),
+(null, null),
+(null, null),
+(null, null),
+(null, null),
+(null, null);
+
+INSERT INTO pledges(amount, user_id, project_id) VALUES
+(500.5, 1, 1),
+(20.20, 2, 2),
+(null, null, null),(null, null, null),(null, null, null),(null, null, null),
+(null, null, null),(null, null, null),(null, null, null),(null, null, null),
+(null, null, null),(null, null, null),(null, null, null),(null, null, null),
+(null, null, null),(null, null, null),(null, null, null),(null, null, null),
+(null, null, null),(null, null, null),(null, null, null),(null, null, null),
+(null, null, null),(null, null, null),(null, null, null),(null, null, null),
+(null, null, null),(null, null, null),(null, null, null),(null, null, null);
